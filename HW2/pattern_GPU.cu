ef5648abb0faf96767732962aa4bacb68931ed4a
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-01-29 19:22:47 +0530

    Adjusting threads/block for optimality

--- HW2/pattern_GPU.cu
+++ HW2/pattern_GPU.cu
@@ -9,25 +9,28 @@
 #include <ctime>
 
 #define LINEWIDTH 20
-#define CHUNKSIZE 32
+#define KEYWORD 32
+#define CHUNKSIZE 8
+
 
 __global__ void matchPattern(unsigned int *text, unsigned int *words, int *matches, int length){
 
 	__shared__ unsigned int shared_words[CHUNKSIZE+1];
-	__shared__ unsigned int data_chunk[CHUNKSIZE][4];
-	__shared__ unsigned int keywords[CHUNKSIZE];
-	unsigned int word, next_word;
+	__shared__ unsigned int keywords[KEYWORD];
+	__shared__  int frequencies[KEYWORD];
+	unsigned int word, next_word, offset1, offset2, offset3;
 
 	int i = (blockIdx.x * blockDim.x) + threadIdx.x;
 	int match_count = 0;
 
 	if(i<length){
-		// Load keywords
+		// Load keywords, initialize blockwise frequencies
 		if(threadIdx.x==0){
 			keywords[threadIdx.y] = words[threadIdx.y];
+			frequencies[threadIdx.y] = 0;
 		}
-		__syncthreads();
 
+		// Load text
 		if(threadIdx.y==0){
 			shared_words[threadIdx.x] = text[i];
 			if(threadIdx.x+1==CHUNKSIZE){
@@ -36,31 +39,33 @@ __global__ void matchPattern(unsigned int *text, unsigned int *words, int *match
 		}
 		__syncthreads();
 
-		// Load text
-		if(threadIdx.y==0){
-			word = shared_words[threadIdx.x];
-			next_word = shared_words[threadIdx.x+1];
-			data_chunk[threadIdx.x][0] = word;
-			data_chunk[threadIdx.x][1] = (word>>8) + (next_word<<24);
-			data_chunk[threadIdx.x][2] = (word>>16) + (next_word<<16);
-			data_chunk[threadIdx.x][3] = (word>>24) + (next_word<<8);
-		}
+		// Matching logic
+		word = shared_words[threadIdx.x];
+		next_word = shared_words[threadIdx.x+1];
+		offset1 = (word>>8) + (next_word<<24);
+		offset2 = (word>>16) + (next_word<<16);
+		offset3 = (word>>24) + (next_word<<8);
+		match_count += (word==keywords[threadIdx.y]);
+		match_count += (offset1==keywords[threadIdx.y]);
+		match_count += (offset2==keywords[threadIdx.y]);
+		match_count += (offset3==keywords[threadIdx.y]);
+
+		// Increment shared counters
+		atomicAdd(&frequencies[threadIdx.y], match_count);
 		__syncthreads();
 
-		match_count += (data_chunk[threadIdx.x][0]==keywords[threadIdx.y]);
-		match_count += (data_chunk[threadIdx.x][1]==keywords[threadIdx.y]);
-		match_count += (data_chunk[threadIdx.x][2]==keywords[threadIdx.y]);
-		match_count += (data_chunk[threadIdx.x][3]==keywords[threadIdx.y]);
-
-		atomicAdd(&matches[threadIdx.y], match_count);
+		// Incrememnt global histogram
+		if(threadIdx.x == 0){
+			atomicAdd(&matches[threadIdx.y], frequencies[threadIdx.y]);
+		}
 	}
 }
 
 int main(){
 
-	int length, len, nwords=32, matches[nwords];
+	int length, len, matches[KEYWORD];
 	int* cuda_matches;
-	char *ctext, keywords[nwords][LINEWIDTH], *line;
+	char *ctext, keywords[KEYWORD][LINEWIDTH], *line;
 	cudaMallocHost(&line, sizeof(char)*LINEWIDTH);
 	unsigned int *text,  *words;
 	unsigned int *cuda_text, *cuda_words;
@@ -71,7 +76,7 @@ int main(){
 	wfile = fopen("./data/keywords.txt","r");
 	if (!wfile){ printf("keywords.txt: File not found.\n");	exit(0);}
 
-	int k=0, cnt = nwords;
+	int k=0, cnt = KEYWORD;
 	size_t read, linelen = LINEWIDTH;
 	while((read = getline(&line, &linelen, wfile)) != -1 && cnt--){
 		strncpy(keywords[k], line, sizeof(line));
@@ -99,9 +104,9 @@ int main(){
 	text = (unsigned int *) ctext;
 
 	// define words for matching
-	cudaMallocHost(&words, nwords*sizeof(unsigned int));
+	cudaMallocHost(&words, KEYWORD*sizeof(unsigned int));
 
-	for (int w=0; w<nwords; w++){
+	for (int w=0; w<KEYWORD; w++){
 		words[w] = ((unsigned int) keywords[w][0])
 			+ ((unsigned int) keywords[w][1])*(1<<8)
 			+ ((unsigned int) keywords[w][2])*(1<<16)
@@ -109,29 +114,24 @@ int main(){
 	}
 
 	cudaMalloc((void**) &cuda_text, length+4);
-	cudaMalloc((void**) &cuda_words, nwords*sizeof(unsigned int));
+	cudaMalloc((void**) &cuda_words, KEYWORD*sizeof(unsigned int));
 	cudaMalloc((void**) &cuda_matches, sizeof(matches));
 
 	cudaMemcpy(cuda_text, text, length+4, cudaMemcpyHostToDevice);
-	cudaMemcpy(cuda_words, words, nwords*sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaMemcpy(cuda_words, words, KEYWORD*sizeof(unsigned int), cudaMemcpyHostToDevice);
 	cudaMemset(cuda_matches, 0, sizeof(matches));
 	cudaMemset(cuda_matches, 0, sizeof(matches));
 
-	dim3 threadsPerBlock(32, 32);
-	dim3 numBlocks(len/32);	
-
-	dim3 threadsPerBlock2(32);
-	dim3 numBlocks2(len/32);
+	dim3 threadsPerBlock(CHUNKSIZE, KEYWORD);
+	dim3 numBlocks(len/CHUNKSIZE);
 
 	matchPattern<<<numBlocks, threadsPerBlock>>>(cuda_text, cuda_words, cuda_matches, len);
-
-	// basic<<<numBlocks2, threadsPerBlock2>>>(cuda_text, cuda_matches, len);
 
 	cudaMemcpy(matches, cuda_matches, sizeof(matches), cudaMemcpyDeviceToHost);
 
 	printf("Printing Matches:\n");
 	printf("Word\t  |\tNumber of Matches\n===================================\n");
-	for (int i = 0; i < nwords; ++i)
+	for (int i = 0; i < KEYWORD; ++i)
 		printf("%s\t  |\t%d\n", keywords[i], matches[i]);
 
 	cudaFree(ctext);
